4199c048edb95cfec783f0adc2fd859695e64184
JoeBot <92783935+JoBot23@users.noreply.github.com>
2022-10-18 15:01:44 +0200

    DragNDrop

---
+++ POO Exercice/Piece.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExemplePOO
+{
+    public class Piece
+    {
+        public int superficie;
+        public string nom;
+
+        public Piece(int superficie, string nom)
+        {
+            this.superficie = superficie;
+            this.nom = nom;
+        }
+
+        public override string ToString()
+        {
+            return $"La pièce {nom} fait {superficie}m²";
+        }
+    }
+}
+
+
---
+++ POO Exercice/Proprietaire.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExemplePOO
+{
+    public class Proprietaire
+    {
+        public string nom;
+        public string prenom;
+        public Bien[] biens = new Bien[0];
+
+
+        public Proprietaire(string nom, string prenom, Bien[] biens)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.biens = biens;
+        }
+
+        private string ListeBiens()
+        {
+            string listeBiens = "";
+            foreach (Bien B in biens)
+            {
+                listeBiens += String.Format("- {0} {1} au {2}\n", B.GetType().Name == "Maison" ? "Une" : "Un", B.GetType().Name, B.Adresse);
+            }
+            return listeBiens;
+        }
+
+        public override string ToString()
+        {
+            string toString = String.Format("{1} {0} {2}", nom, prenom, biens?.Length != 0 ? "possède\n" : "ne possède aucun bien");
+            toString += ListeBiens();
+            return toString;
+        }
+    }
+}
+
